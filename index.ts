a4aff0d1a9ae122e3a32b214ab15d4a3ca1f332a
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2024-02-05 18:00:02 +0100

    post done

--- index.ts
+++ index.ts
@@ -1,16 +1,20 @@
 import express from "express";
-import dbConn from "./dbConnection/dbConn"
+import dbConnect from "./dbConnection/dbConn";
+import "dotenv/config";
+import todoRouter from "./routers/todoRouters";
+import cors from "cors";
+
+dbConnect();
 
 const app = express();
-
-dbConn();
-
+app.use(cors());
+app.use(express.json());
+app.use("/todos", todoRouter);
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
-})
-
+  res.send("Hello World");
+});
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-})
+  console.log("Server is running on port 3000");
+});
